f28c7ab4813597e79d1df855d8fcd60e65c29774
Joel Hempel <99513652+J0ELh@users.noreply.github.com>
2024-01-20 13:12:58 -0800

    new stuff

---
+++ client/src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-slate-800 text-gray-300 px-2 sticky top-5 z-100 beginning-rectangle'>
+      <h1 className='font-bold'>Smart Survey AI Tool</h1>
+    </div>
+  )
+}
+
+export default Header
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
+import Header from "./components/Header"
 
 const Home: React.FC = () => {
+
+  const [number, setNumber] = useState(0);
+
+  // Correcting the function declaration
+  const updateNumber = () => {
+    setNumber(number + 1);
+  };
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header />
+      <h1 className='number hover:text-blue-900'>{number}</h1>
+      <button onClick={updateNumber}>Change Number</button>
     </div>
   );
 };
