3d02b338be0a7547519c0d8032a52fc1baa1731e
Joel Hempel <99513652+J0ELh@users.noreply.github.com>
2024-01-20 17:26:24 -0800

    adding stuff to get player icons and play game

---
+++ server/scrape_images.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+base_url = 'https://fifaindex.com/players/fifa16_73/'
+filename = '2016_images.txt'  # File where data will be saved
+page_number = 1
+
+while True:
+    current_page = f"{base_url}?page={page_number}"
+    response = requests.get(current_page)
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+
+    figures = soup.find_all('figure', class_='player')
+    if not figures:
+        break  # Break the loop if no player data is found
+
+    with open(filename, 'a', encoding='utf-8') as file:  # Open file in append mode with UTF-8 encoding
+        for figure in figures:
+            a_tag = figure.find('a', class_='link-player')
+            img_tag = figure.find('img')
+
+            if a_tag and img_tag:
+                title = a_tag.get('title')
+                img_src = img_tag.get('src')
+                file.write(f"{title}, {img_src}\n")  # Write to file
+
+    page_number += 1  # Increment the page number for the next iteration
+    time.sleep(0.4)  # Sleep for 1 second to be polite with the server
--- server/server.py
+++ server/server.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+import functionality
+import json
+
 
 app = FastAPI()
 
@@ -7,3 +10,28 @@ def read_root():
     print('hi')
     
     return {"Hello": "World"}
+
+
+@app.get("/get_data/")
+def load_cards():
+    data = functionality.get_data(2015)
+    cards_u1, cars_u1, all_cards =  functionality.get_cards(data)
+    json_df1 = cards_u1.to_json(orient='records')
+    json_df2 = cars_u1.to_json(orient='records')
+
+    data_df1 = json.loads(json_df1)
+    data_df2 = json.loads(json_df2)
+
+    urls = []
+    for index in range(len(all_cards)):
+
+        urls.append(functionality.get_url(all_cards.iloc[index], 'data/image_links/2015_images.txt'))
+
+    combined_json = {
+        "player_1": data_df1,
+        "player_2": data_df2,
+        "image_links": urls
+    }
+
+
+    return json.dumps(combined_json)
